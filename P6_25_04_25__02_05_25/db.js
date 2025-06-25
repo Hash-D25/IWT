89bfde363ced0f5142f568722e56f7a10d85da37
Harshita Dubey <149588109+Hash-D25@users.noreply.github.com>
2025-06-25 14:38:48 +0530

    Update db.js

--- P6_25_04_25__02_05_25/db.js
+++ P6_25_04_25__02_05_25/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   host: 'localhost',
   user: 'root',         // Replace with your MySQL username
-  password: 'mysqlme@14',         // Replace with your MySQL password
+  password: '*****me***',         // Replace with your MySQL password
   database: 'IWT'
 });
 
